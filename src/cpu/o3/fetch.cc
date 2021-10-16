e89426d41fc18d7af6354937f3dca9945370df63
Gabe Black <gabe.black@gmail.com>
2021-10-16 03:34:37 -0700

    arch,cpu: Use PCStateBase for decoder methods.
    Change-Id: I79f1c5dd39de7015a5c5b891e1888d9a176bb5b4

--- src/cpu/o3/fetch.cc
+++ src/cpu/o3/fetch.cc
@@ -1226,7 +1226,7 @@ Fetch::fetch(bool &status_change)
 
             memcpy(dec_ptr->moreBytesPtr(),
                     fetchBuffer[tid] + blkOffset * instSize, instSize);
-            decoder[tid]->moreBytes(this_pc.as<TheISA::PCState>(), fetchAddr);
+            decoder[tid]->moreBytes(this_pc, fetchAddr);
 
             if (dec_ptr->needMoreBytes()) {
                 blkOffset++;
@@ -1240,8 +1240,7 @@ Fetch::fetch(bool &status_change)
         do {
             if (!(curMacroop || inRom)) {
                 if (dec_ptr->instReady()) {
-                    staticInst = dec_ptr->decode(
-                            this_pc.as<TheISA::PCState>());
+                    staticInst = dec_ptr->decode(this_pc);
 
                     // Increment stat of fetched instructions.
                     ++fetchStats.insts;
